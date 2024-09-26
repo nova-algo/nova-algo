9441d797653eab0337a6e43655418dcfad97ea70
degencodebeast <newfirereloaded@protonmail.com>
2024-09-26 13:41:19 +0100

    feat: added a get_market_index_and_type function to the drift api

--- backend/src/api/drift/api.py
+++ backend/src/api/drift/api.py
@@ -18,7 +18,7 @@ import os
 import json
 import logging
 import asyncio
-from typing import Optional, Union
+from typing import Optional, Tuple, Union
 
 from anchorpy import Wallet # type: ignore
 from solders.keypair import Keypair # type: ignore
@@ -114,62 +114,6 @@ class DriftAPI:
         )
         
         await self.drift_client.initialize_user()
-
-        # # Add the user with the specified subaccount ID and subscribe to updates
-        # await self.drift_client.add_user(subaccount_id)
-        # await self.drift_client.subscribe()
-
-    # async def get_position_and_maxpos(self, market_index, max_positions):
-    #     user = self.drift_client.get_user()
-    #     positions = []
-    #     open_positions = []
-
-    #     for position in user.positions:
-    #         if position.market_index == market_index and position.base_asset_amount != 0:
-    #             positions.append(position)
-    #             open_positions.append(position.market)
-
-    #     num_of_pos = len(open_positions)
-
-    #     if len(open_positions) > max_positions:
-    #         logger.info(f'We are in {len(open_positions)} positions and max pos is {max_positions}... closing positions')
-    #         for position in open_positions:
-    #              await self.kill_switch(position.market_index)
-    #     else:
-    #         logger.info(f'We are in {len(open_positions)} positions and max pos is {max_positions}... not closing positions')
-
-    #     if positions:
-    #         position = positions[0]
-    #         in_pos = True
-    #         size = position.base_asset_amount
-    #         pos_sym = position.market
-    #         entry_px = calculate_entry_price(position)
-    #         pnl_perc = position.unrealized_pnl_percentage * 100
-    #         long = size > 0
-    #     else:
-    #         in_pos = False
-    #         size = 0
-    #         pos_sym = None
-    #         entry_px = 0
-    #         pnl_perc = 0
-    #         long = None
-
-    #     return positions, in_pos, size, pos_sym, entry_px, pnl_perc, long, num_of_pos
-
-    # async def adjust_leverage_size_signal(self, market_index, leverage):
-    #     user = self.drift_client.get_user()
-    #     acct_value = float(user.collateral)
-    #     acct_val95 = acct_value * 0.95
-
-    #     await self.drift_client.set_leverage(market_index, leverage)
-
-    #     market = self.drift_client.get_market(market_index)
-    #     price = float(market.oracle_price)
-
-    #     size = (acct_val95 / price) * leverage
-    #     size = round(size, market.base_precision)
-
-    #     return leverage, size
     
     async def cancel_orders_for_market(self, market_type, market_index, subaccount_id: Optional[Pubkey] = None):
         """
@@ -350,28 +294,59 @@ class DriftAPI:
 
         return position
 
-    async def get_market_index_by_symbol(self, symbol):
+    def get_market_index_and_type(self, name: str) -> Tuple[int, MarketType]:
         """
-        Retrieves the market index for a given symbol.
+        Retrieves market index and type for a given market name.
 
         Args:
-            symbol (str): The market symbol.
+            name (str): The name of the market.
 
         Returns:
-            int: The market index corresponding to the given symbol.
+            Tuple[int, MarketType]: A tuple containing the market index and market type.
         """
-        return await self.drift_client.get_market_index_by_symbol(symbol)
+        return self.drift_client.get_market_index_and_type(name)
+    
 
-    def get_market(self, market_index):
-        """
-        Retrieves market information for a given market index.
 
-        Args:
-            market_index (int): The market index.
+            # # Add the user with the specified subaccount ID and subscribe to updates
+        # await self.drift_client.add_user(subaccount_id)
+        # await self.drift_client.subscribe()
 
-        Returns:
-            Market: The market information for the given index.
-        """
-        return self.drift_client.get_market(market_index)
+    # async def get_position_and_maxpos(self, market_index, max_positions):
+    #     user = self.drift_client.get_user()
+    #     positions = []
+    #     open_positions = []
+
+    #     for position in user.positions:
+    #         if position.market_index == market_index and position.base_asset_amount != 0:
+    #             positions.append(position)
+    #             open_positions.append(position.market)
+
+    #     num_of_pos = len(open_positions)
+
+    #     if len(open_positions) > max_positions:
+    #         logger.info(f'We are in {len(open_positions)} positions and max pos is {max_positions}... closing positions')
+    #         for position in open_positions:
+    #              await self.kill_switch(position.market_index)
+    #     else:
+    #         logger.info(f'We are in {len(open_positions)} positions and max pos is {max_positions}... not closing positions')
+
+    #     if positions:
+    #         position = positions[0]
+    #         in_pos = True
+    #         size = position.base_asset_amount
+    #         pos_sym = position.market
+    #         entry_px = calculate_entry_price(position)
+    #         pnl_perc = position.unrealized_pnl_percentage * 100
+    #         long = size > 0
+    #     else:
+    #         in_pos = False
+    #         size = 0
+    #         pos_sym = None
+    #         entry_px = 0
+    #         pnl_perc = 0
+    #         long = None
+
+    #     return positions, in_pos, size, pos_sym, entry_px, pnl_perc, long, num_of_pos
     
     
