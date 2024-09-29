015daddc953dc51592cf3dac34b1ff11553ee828
Lucky Victory <luckyvictory54@gmail.com>
2024-09-29 23:41:54 +0100

    added markdown renderer

---
+++ frontend/src/components/SectionHeading.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { HStack, Text } from "@chakra-ui/react";
+
+export default function SectionHeading({
+  title,
+  containerStyleProps,
+  textStyleProps,
+  hideBorder,
+}: {
+  title: string;
+  hideBorder?: boolean;
+  containerStyleProps?: Record<string, any>;
+  textStyleProps?: Record<string, any>;
+}) {
+  return (
+    <HStack
+      my={5}
+      pos={"relative"}
+      pb={2}
+      {...(!hideBorder && {
+        _before: {
+          content: `''`,
+          w: "50px",
+          h: "3px",
+          rounded: "full",
+          bg: "orange.500",
+          pos: "absolute",
+          bottom: "0",
+          left: 0,
+        },
+      })}
+      {...containerStyleProps}
+    >
+      <Text
+        as={"span"}
+        fontSize={{ base: "18px", md: "22px" }}
+        fontWeight={"bold"}
+        {...textStyleProps}
+      >
+        {title}
+      </Text>
+    </HStack>
+  );
+}
